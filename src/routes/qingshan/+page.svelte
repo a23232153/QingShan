a5b17e398cfa781d8c326d6b4a8f114f92863ee7
kai <zx23232153zx@gmail.com>
2025-11-30 14:19:59 +0800

    1130 i

--- src/routes/qingshan/+page.svelte
+++ src/routes/qingshan/+page.svelte
@@ -138,9 +138,9 @@
     <!-- Hero  -->  
     <div class="relative z-10 flex flex-col items-center justify-center h-full px-6">
       <h1 class="md:text-6xl font-bold leading-tight drop-shadow-xl backdrop-blur border-yellow-500 border-b-4 pb-2 mb-2 py-2 px-2">
-        青山 qingshan
+        艋舺青山宮介紹
       </h1>
-      <p class="mt-8 text-lg md:text-2xl backdrop-blur">艋舺青山宮介紹</p>
+      <p class="mt-8 text-lg md:text-2xl backdrop-blur">走進信仰與歷史交織的聖地</p>
       
     </div>
   </section>
